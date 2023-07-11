fd14a11b7eb2681e299f922c11d2e2a632f7fc6d
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-07-11 20:39:32 +0530

    [Amit]Add : Validating First Name

---
+++ User_Registration/User_Registration/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace User_Registration
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Validator validator = new Validator();
+            bool doOrNot = true;
+            while (doOrNot)
+            {
+                Console.WriteLine("Chose an Option");
+                Console.WriteLine("1. Registration of First name");
+                Console.WriteLine("0. Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 0:
+                        doOrNot= false;
+                        break;
+                    case 1:
+                        validator.FirstNameCheck();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ User_Registration/User_Registration/Validator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace User_Registration
+{
+    public class Validator
+    {
+        public bool Validate(string input, string pattern)
+        {
+            return Regex.IsMatch(input, pattern);
+        }
+        public void FirstNameCheck()
+        {
+            bool check = true;
+            while (check)
+            {
+                Console.WriteLine("Enter First Name");
+                string firstName = Console.ReadLine();
+                string fstNamePattern = "^[A-Z][A-Za-z]{2,}$";
+                if (Validate(firstName, fstNamePattern))
+                {
+                    Console.WriteLine($"The First Name of The Person is {firstName}");
+                    check = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter a valid FirstName Where first letter must be in capital and it contain minimun three charectar");
+                }
+            }
+        }
+    }
+}
